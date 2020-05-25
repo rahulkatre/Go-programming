72a0e5b44afe0ed2543ffd30541724c1e3a4a741
rahul katre <rahulkatre@rahuls-MacBook-Pro.local>
2020-05-25 20:02:40 +0530

    control flow execrcises

--- control-flow/exercise_two.go
+++ control-flow/exercise_two.go
@@ -9,4 +9,15 @@ func main() {
 			fmt.Printf("%#U\n", i)
 		}
 	}
+	// If expression not provided to swith keyword, it wil by default evaluates for true value
+	switch {
+	case 1==1:
+		fmt.Println("Equal")
+		// fallthrough lets others cases to be executed no matter whether the cases passes or fails.
+		fallthrough
+	case  !true:
+		fmt.Println("Not Equal")
+	default:
+		fmt.Println("Default case")
+	}
 }
